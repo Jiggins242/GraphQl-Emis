f228abd4d4bedf0486a4a361e319a12a4666300f
Jiggins242 <44429273+Jiggins242@users.noreply.github.com>
2018-12-17 13:39:42 +0000

    mongo db working locally of a docker container

--- server/app.js
+++ server/app.js
@@ -1,50 +1,41 @@
 const { GraphQLServer } = require ('graphql-yoga');
 const { Prisma } = require ('prisma-binding');
 
-// dummy data //
-let patients = [{
-    id: 'id-0',
-    title: 'Mr',
-    forname: 'Homer',
-    surname: 'Simpson',
-    age: '39',
-    nhsnum: '12345',
-}]
-let idCount = patients.length
 const resolvers = {
     Query: {
-        info: () => 'text',
-        patientInfo: () => patients
+        info: () => 'Testing Testing 1 2 3 ',
+        patientInfo: (parent, args, context, info) => {
+            return context.db.query.patients({}, info)
+        } 
     },
 
     Mutation: {
-        post: (parent, args) => {
-            const patient ={
-                id: `id-${idCount++}`,
-                title: args.title,
-                forname: args.forname,
-                surname: args.surname,
-                age: args.age,
-                nhsnum: args.nhsnum
-            }
-            patients.push(patient)
-            return patient
-        }
+        post: (parent, args, context, info) => {
+            return context.db.mutation.createPatient({
+                data: {
+                    title: args.title,
+                    forname: args.forname,
+                    surname: args.surname,
+                    age: args.age,
+                    nhsnum: args.nhsnum,
+                },
+            }, info)
+        },
     },
-
-    Patient: {
-        id: (parent) => parent.id,
-        title: (parent) => parent.title,
-        forname: (parent) => parent.forname,
-        surname: (parent) => parent.surname,
-        age: (parent) => parent.age,
-        nhsnum: (parent) => parent.nhsnum
-    }
 }
 
 const server = new GraphQLServer({
-    typeDefs: './schema/schema.graphql',
-    resolvers
+    typeDefs: './src/schema.graphql',
+    resolvers,
+    context: req => ({
+        ...req,
+        db: new Prisma({
+            typeDefs:'src/generated/prisma.graphql',
+            endpoint: 'http://localhost:4466',
+            //secret: 'mysecret123',
+            debug: true,
+        })
+    })
 })
 
 server.start(4000, () => {
